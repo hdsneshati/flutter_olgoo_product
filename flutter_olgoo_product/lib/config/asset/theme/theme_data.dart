365a9e9e3f1ee9d3ace196fac10777923e5b6efe
hdsneshati <hadisneshati1384@gmail.com>
2025-08-30 04:44:11 +0330

    add theme

---
+++ flutter_olgoo_product/lib/config/asset/theme/theme_data.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_olgoo_product/config/asset/theme/color_pallet.dart';
+import 'package:flutter_olgoo_product/config/asset/theme/text_styles.dart';
+
+ThemeData lightThemeData = ThemeData(
+    fontFamily: 'dana',
+    colorScheme: ColorPallet.lightColorScheme,
+    textTheme: lightTextTheme,
+    useMaterial3: true,
+    );
