441edf82cb245382d4444f956a06aec43da74dd1
hdsneshati <hadisneshati1384@gmail.com>
2025-08-30 06:15:59 +0330

    add localissation in main

--- flutter_olgoo_product/lib/main.dart
+++ flutter_olgoo_product/lib/main.dart
@@ -1,35 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localisation/flutter_localisation.dart';
 import 'package:flutter_olgoo_product/config/asset/theme/theme_data.dart';
 import 'package:flutter_olgoo_product/config/routing/routes.dart';
 import 'package:flutter_olgoo_product/core/dependency_injection/locator.dart';
 import 'package:flutter_olgoo_product/feature/presentation/authentication/bloc/auth/auth.bloc.dart';
+import 'package:persian_datetime_picker/persian_datetime_picker.dart';
 import 'package:size_config/size_config.dart';
-
+import 'package:persian/persian.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
- 
+  await _flutterLocalisationService.initialize();
 
   await locatorSetup();
 
- 
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-  ]);
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
-  runApp(MultiBlocProvider(
-    providers: [
-   
-     BlocProvider(
-      create: (context) => AuthBloc(locator()),
+  runApp(
+    MultiBlocProvider(
+      providers: [BlocProvider(create: (context) => AuthBloc(locator()))],
+      child: const MyApp(),
     ),
-   
-  ], child: const MyApp()));
+  );
 }
-
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -50,10 +44,25 @@ class MyApp extends StatelessWidget {
             // Locale('en', 'US'),
           ],
           debugShowCheckedModeBanner: false,
-         
+          localizationsDelegates: const [
+            PersianMaterialLocalizations.delegate,
+            PersianCupertinoLocalizations.delegate,
+           // GlobalMaterialLocalizations.delegate,
+           // GlobalWidgetsLocalizations.delegate,
+            // GlobalCupertinoLocalizations.delegate,
+          ],
         );
       },
     );
   }
 }
 
+final _flutterLocalisationService = TranslationService(
+  config: const TranslationConfig(
+    enableLogging: true,
+    secretKey: "sk_live_Z095M95SsXi-LqI39vduaa9Vhc-Zjug8oXjnST-DGNs",
+    supportedLocales: ['en', 'es'],
+    projectId: 31,
+    flavorName: 'Default',
+  ),
+);
